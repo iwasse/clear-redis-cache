f2a1d225086effe9a0be5a4df5fe1b9ca6f75d33
Guilherme Iwasse <guilherme.iwasse@gmail.com>
2021-07-19 23:09:40 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,109 @@
+const redis = require("redis")
+const express = require("express");
+const app = express();
+require('dotenv').config()
+
+
+app.get('/', (req, res) => {
+    res.json({'Message': 'conectado'})
+})
+
+app.get('/api/financings/clear-cache', (req, res) => {
+        const client = redis.createClient({
+            host: process.env.HOST_FINANCINGS,
+            port: process.env.REDIS_PORT_FINANCINGS,
+            password: process.env.REDIS_PASSWORD_FINANCINGS,
+            tls: true
+        })
+        
+        client.flushdb(function(err, success){
+            if(success){
+            console.log("Financings - flushdb: " + success)
+                res.send({
+                    'cache': 'Financings',
+                    'message': 'Successfully cleared cache',
+                    'status': success
+                })
+            }
+            else
+            console.log('Error ' + err)
+        })
+         
+        client.on('error', err => {
+            console.log('Error ' + err);
+        });
+
+        client.quit()
+
+})
+
+
+app.get('/api/loans/clear-cache', (req, res) => {
+        const client = redis.createClient({
+            host: process.env.HOST_LOANS,
+            port: process.env.REDIS_PORT_LOANS,
+            password: process.env.REDIS_PASSWORD_LOANS,
+            tls: true
+        })
+        
+        client.flushdb(function(err, success){
+            if(success){
+                console.log("Loans - flushdb: " + success)
+                res.send({
+                    'cache': 'Loans',
+                    'message': 'Successfully cleared cache',
+                    'status': success
+                })
+            }
+            else
+            console.log('Error ' + err)
+        })
+         
+        client.on('error', err => {
+            console.log('Error ' + err);
+        });
+        
+        client.quit()
+    
+})
+
+
+app.get('/api/unarranged-accounts-overdraft/clear-cache', (req, res) => {
+        const client = redis.createClient({
+            host: process.env.HOST_UNARRANGED,
+            port: process.env.REDIS_PORT_UNARRANGED,
+            password: process.env.REDIS_PASSWORD_UNARRANGED,
+            tls: true
+        })
+        
+        client.flushdb(function(err, success){
+            if(success){
+                console.log("Unarranged Accounts Overdraft - flushdb: " + success)
+                res.send({
+                    'cache': 'Unarranged Accounts Overdraft',
+                    'message': 'Successfully cleared cache',
+                    'status': success
+                })
+            }
+            else
+            console.log('Error ' + err)
+        })
+         
+        client.on('error', err => {
+            console.log('Error ' + err);
+        });
+        
+        client.quit()
+    
+})
+
+const port = process.env.PORT
+app.listen(port, () =>{
+    console.log(`Listening on port ${port}...`)
+})
+
+
+
+
+
+
